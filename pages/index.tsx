3da709eecb12a2773d3e1eb7e97e4f61316c0a5e
katomitsu <katomitsu0925@@gmail.com>
2025-05-04 15:57:09 +0900

    Add index page to fix 404

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// anon-board/pages/index.tsx
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>5ch風 匿名掲示板</h1>
+      <p>ようこそ。ここから開発を始めましょう！</p>
+    </main>
+  );
+}
