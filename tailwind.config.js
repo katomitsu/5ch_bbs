d6e920f61cfd9afec92ebcb09727537c249f54ad
katomitsu <katomitsu0925@@gmail.com>
2025-05-05 11:51:24 +0900

    tailwind.config.js に app ディレクトリを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+  + "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
+  
   theme: {
     container: {
       center: true,
